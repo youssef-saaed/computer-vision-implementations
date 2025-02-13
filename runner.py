a6839152a8236f07561d86a0edea95fb8ec83237
Yousef Said <youssef.saaed.04@gmail.com>
2025-02-13 16:39:59 +0200

    Added command line runner for filters

--- runner.py
+++ runner.py
@@ -1,3 +1,19 @@
 from spatial_filters import *
+import sys
+import os
 
-negative_filter("./input/increase_brightness.jpg", "./output/increase_brightness.jpg")
+filter_names = {
+    "negative": negative_filter
+}
+
+def main(argv):
+    filter_names[argv[1]](f"./input/{argv[2]}", f"./output/{argv[2]}")
+    
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print(f"Usage: {os.path.relpath(__file__)} [Filter Name] [Image File Name]")
+        sys.exit() 
+    if sys.argv[1] not in filter_names:
+        print("Filter doesn't exist")
+        sys.exit()
+    main(sys.argv)
