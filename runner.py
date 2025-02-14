7863eb6855c89dc0b957452892c08e890bc07f33
Yousef Said <youssef.saaed.04@gmail.com>
2025-02-14 14:17:13 +0200

    Added gamma correction filter + changed cmd runner to accept optional arguments related to each filter + changed the architecture of my implementation by seperating reading and writing images from the filters logic to ensure scalability

--- runner.py
+++ runner.py
@@ -2,16 +2,12 @@ from spatial_filters import *
 import sys
 import os
 
-filter_names = {
-    "negative": negative_filter
-}
-
 def main(argv):
-    filter_names[argv[1]](f"./input/{argv[2]}", f"./output/{argv[2]}")
+    apply_filter(argv[1], f"./input/{argv[2]}", f"./output/{argv[2]}", *argv[3:])
     
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print(f"Usage: {os.path.relpath(__file__)} [Filter Name] [Image File Name]")
+    if len(sys.argv) < 3:
+        print(f"Usage: {os.path.relpath(__file__)} [Filter Name] [Image File Name] [Optional Arguments]")
         sys.exit() 
     if sys.argv[1] not in filter_names:
         print("Filter doesn't exist")
--- spatial_filters/filters.py
+++ spatial_filters/filters.py
@@ -1,7 +1,21 @@
 from .utils import * 
+import numpy as np
 
-def negative_filter(image_path: str, output_path: str) -> None:
-    image = get_image(image_path)
+def negative_filter(image: np.ndarray, *_: None) -> np.ndarray:
     transformed_image = 255 - image
-    save_image(transformed_image, output_path)
+    return transformed_image
     
+def gamma_correction_filter(image: np.ndarray, gamma) -> np.ndarray:
+    transformed_image = (image / 255) ** float(gamma) * 255
+    clipped_image = transformed_image.clip(0, 255)
+    return clipped_image
+    
+filter_names = {
+    "negative": negative_filter, 
+    "gamma": gamma_correction_filter,
+}
+
+def apply_filter(name: str, image_path: str, output_path: str, *args):
+    image = get_image(image_path)
+    output_image = filter_names[name](image, *args)
+    save_image(output_image, output_path)
