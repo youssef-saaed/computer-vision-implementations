8dd1481994c5a0afe00c7ae82f2bdbedc43003da
Yousef Said <youssef.saaed.04@gmail.com>
2025-02-15 06:10:21 +0200

    Added helper function called add_padding which will help in applying sliding kernel filter

--- spatial_filters/filters.py
+++ spatial_filters/filters.py
@@ -1,5 +1,8 @@
 import numpy as np
+from collections import defaultdict
+from typing import Any
 import cv2
+from copy import deepcopy
 
 def __get_image(image_path: str) -> np.ndarray:
     image = cv2.imread(image_path)
@@ -8,19 +11,55 @@ def __get_image(image_path: str) -> np.ndarray:
 def __save_image(image: np.ndarray, save_location: str) -> None:
     image = image.astype(np.uint8)
     cv2.imwrite(save_location, image)
+    
+def freq_dict_increment(dict: defaultdict, key: Any) -> None:
+    dict[key] += 1
+
+def add_padding(image: np.ndarray, padding_size: int) -> np.ndarray:
+    padded_image_shape = (image.shape[0] + 2 * padding_size, image.shape[1] + 2 * padding_size, image.shape[2])
+    padded_image = np.zeros(padded_image_shape, dtype=int)
+    padded_image[padding_size: -padding_size, padding_size: -padding_size, :] = image[:, :, :]
+    return padded_image
+
+
 
 def negative_filter(image: np.ndarray, *_: None) -> np.ndarray:
     transformed_image = 255 - image
     return transformed_image
     
-def gamma_correction_filter(image: np.ndarray, gamma) -> np.ndarray:
+def gamma_correction_filter(image: np.ndarray, gamma: Any) -> np.ndarray:
     transformed_image = (image / 255) ** float(gamma) * 255
     clipped_image = transformed_image.clip(0, 255)
     return clipped_image
+
+def histogram_equalizer_filter(image: np.ndarray, alpha: Any = 1) -> np.ndarray:
+    alpha = float(alpha)
+    
+    freq = [defaultdict(int), defaultdict(int), defaultdict(int)]
+    
+    for i in range(3):
+        np.vectorize(lambda val: freq_dict_increment(freq[i], val))(image[:, :, i])
+    
+    mapping = [{}, {}, {}]
+    cumulative_prob = [0, 0, 0]
+    for i in range(256):
+        for j in range(3):
+            freq[j][i] /= image.shape[0] * image.shape[1]
+            cumulative_prob[j] += freq[j][i]
+            mapping[j][i] = cumulative_prob[j] * 255
+    
+    equalized_image = deepcopy(image)
+    for i in range(3):
+        equalized_image[:, :, i] = np.vectorize(lambda val: mapping[i][val])(image[:, :, i])
+    
+    return (1 - alpha) * image + alpha * equalized_image
+    
+    
     
 filter_names = {
     "negative": negative_filter, 
     "gamma": gamma_correction_filter,
+    "histeq": histogram_equalizer_filter,
 }
 
 def apply_filter(name: str, image_path: str, output_path: str, *args):
